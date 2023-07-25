286402db941ed3b5a7608878c2077555141c6789
Chingis <chingiskarbushov@live.ru>
2023-07-24 23:27:39 -0400

    initial commit

---
+++ booking/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class User
+
+
+class Booking(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    room = models.ForeignKey('Room', on_delete=models.CASCADE)
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+    num_guests = models.PositiveIntegerField()
+
+    # Add more fields as per your application's requirements
+
+    def __str__(self):
+        return f"{self.user} - {self.room} - {self.check_in_date} to {self.check_out_date}"
+
+
+class Payment(models.Model):
+    booking = models.OneToOneField('Booking', on_delete=models.CASCADE)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    # Add more fields as per your application's requirements
+
+    def __str__(self):
+        return f"Booking: {self.booking} - Amount: {self.amount}"
---
+++ little_elista/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for little_elista project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "little_elista.settings")
+
+application = get_asgi_application()
