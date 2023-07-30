b2f7f2cd37c171292040dad5db6b25698294011f
Chingis <chingiskarbushov@live.ru>
2023-07-30 01:39:13 -0400

    Zones admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from booking.models import Booking, Payment, Table
+from booking.models import Booking, Payment, Table, Zone
 
 admin.site.register(Booking)
 admin.site.register(Payment)
+admin.site.register(Zone)
 admin.site.register(Table)
